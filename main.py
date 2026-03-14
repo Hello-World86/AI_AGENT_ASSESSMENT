e6ad63d8550eb9ca7f47b38446979c25abbda00d
Hello-World86 <luniva086@gmail.com>
2026-03-14 20:40:08 +0545

    Integrated everything in main

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from fastapi import FastAPI, HTTPException
-import agent from agent
-app = FastAPI()
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, List, Dict
+
+from agent import run_agent
+
+app = FastAPI(
+    title = "Customer Support AI Agent",
+    description = "An AI agent that assists customer support teams by retrieving order details, customer profiles, and relevant policy information to provide accurate responses to customer inquiries.",
+    version = "1.0"
+)
+
+class ChatRequest(BaseModel):
+    query: str
+    chat_history: Optional[List[Dict[str,str]]] = None
+
+class ChatResponse(BaseModel):
+    answer: str
+    steps: list[Dict[str, str]]
+    iterations: int
 
 @app.get("/")
 def root():
-    return{"messege": "API is running"}
+    return{"messege": "Customer Support API is running."}
 
-@app.post("/chat")
-def QueryRequest(query:str):
-    response = agent(query)
-from fastapi import FastAPI
-from models import ChatRequest
-from agent import run_agent
-
-app = FastAPI()
-
-
-@app.post("/chat")
+@app.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
 
-    answer = run_agent(request.query)
+    result = run_agent(
+        query = request.query,
+        chat_history = request.chat_history
+    )
 
-    return {"answer": answer}
+    return ChatResponse(
+        answer = result.get("answer", "Sorry, No answer generated."),
+        steps = result.get("steps", []),
+        iterations = result.get("iterations", 0)
+    )
